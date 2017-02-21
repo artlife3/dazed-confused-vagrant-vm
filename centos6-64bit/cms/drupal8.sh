1e4ee954438577acb2f0a7e129138a10dd8b9f50
artlife3 <artlife3@mac.com>
2017-02-22 05:40:21 +0900

    Drupal8

---
+++ centos6-64bit/cms/drupal8.sh
@@ -0,0 +1,18 @@
+su vagrant
+umask 002
+
+
+#Drupal
+mysql -u root -proot -e "CREATE DATABASE drupal;"
+cd /var/www/html/
+wget https://ftp.drupal.org/files/projects/drupal-8.2.6.tar.gz
+tar xvzf drupal-8.2.6.tar.gz
+ln -s drupal-8.2.6 drupal
+
+cd /var/www/html/drupal
+mkdir sites/default/files
+sudo chmod a+w sites/default/files
+cp sites/default/default.settings.php sites/default/settings.php
+sudo chown -R apache:vagrant /var/www/html/drupal-8.2.6
+
+
