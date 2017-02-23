1a5ac94eca5336a5c9dd3190ab49c534674c26a6
artlife3 <artlife3@mac.com>
2017-02-24 02:21:07 +0900

    Change LANG to EN

--- centos6-64bit/provision/middleware/location.sh
+++ centos6-64bit/provision/middleware/location.sh
@@ -5,3 +5,8 @@ UTC="false"
 EOT
 source /etc/sysconfig/clock
 /bin/cp -f /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+
+export LANG=en_US.UTF-8
+sudo sed -i -e "s/LANG=\"de_DE.UTF-8\"/LANG=\"en_US.UTF-8\"/g" /etc/sysconfig/i18n
+
+
