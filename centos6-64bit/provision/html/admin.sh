905d16a6e1985a2b71ece33000bea3f8637da01c
artlife3 <artlife3@mac.com>
2017-02-27 02:16:16 +0900

    Add mailcatcher #2

--- centos6-64bit/provision/html/admin.sh
+++ centos6-64bit/provision/html/admin.sh
@@ -139,9 +139,6 @@ var_dump(\$get_result);
 ?>
 EOT
 
-
-
-
 #apcu
 cd /var/www/html/admin
 git clone https://github.com/krakjoe/apcu.git
@@ -153,20 +150,19 @@ git clone https://github.com/rlerdorf/opcache-status.git
 # mail_test.php
 cat << EOT > /var/www/html/admin/mail_test.php
 <?php
-/*
+
 mb_language("ja");
 mb_internal_encoding("UTF-8");
 
-\$to = "artlife3@mac.com";
-\$from = "artlife3@mac.com";
+\$to = "webmaster-to@example.com";
+\$from = "webmaster-from@example.com";
+
 \$subject = "テストのサブジェクト";
 \$body = "テスト\nテストテスト\nテストテストテスト\nテストテスト\nテスト";
 
 mb_send_mail(\$to,\$subject,\$body,"From:".\$from);
-*/
 
-\$to = "artlife3@mac.com";
-\$from = "artlife3@mac.com";
+
 \$subject = "TEST SUBJECT";
 \$body = "TEST BODY";
 
@@ -180,7 +176,93 @@ if(\$result){
 }
 
 EOT
+# mail-test.php
+cat << EOT > /var/www/html/admin/mail-test.php
+<?php
 
+	/*
+	DONT FORGET TO DELETE THIS SCRIPT WHEN FINISHED!
+	*/
+
+	ini_set( 'display_errors', 1 );
+	error_reporting( E_ALL );
+	
+	\$from = 'webmaster@example.com';
+	
+	/*
+	ini_set( 'SMTP', 'smtp.example.com' );
+	ini_set( 'SMTP_PORT', 25 );
+	ini_set( 'sendmail_from', \$from );
+	*/
+	
+	\$server = array( 
+		'HTTP_HOST', 'SERVER_NAME', 'SERVER_ADDR', 'SERVER_PORT',
+ 		'SERVER_ADMIN', 'SERVER_SIGNATURE', 'SERVER_SOFTWARE', 
+		'REMOTE_ADDR', 'DOCUMENT_ROOT', 'REQUEST_URI', 
+		'SCRIPT_NAME', 'SCRIPT_FILENAME',
+	);
+	
+	\$to      = ( isset( \$_GET['email'] ) ? \$_GET['email'] : FALSE );
+	\$subject = 'Mail Test Successful for ' . \$_SERVER['HTTP_HOST'];
+	\$message = '';
+	
+	if ( ! \$to )
+	{
+		echo '<strong>Set \$_GET[\'email\'].</strong>';
+		exit;
+	};
+	
+	foreach ( \$server as \$s )
+	{
+		\$message .= sprintf( '%s: %s', \$s, \$_SERVER[\$s] ) . PHP_EOL;
+	};
+	
+	\$headers = 'From: ' . \$from . PHP_EOL 
+		 . 'Reply-To: ' . \$from . PHP_EOL 
+		 . 'X-Mailer: PHP/' . phpversion(); 
+	
+	if ( isset( \$_GET['send'] ) && \$_GET['send'] === 'true' )
+	{					
+		\$success = mail( \$to, \$subject, \$message, \$headers );
+	}
+	else
+	{
+		echo '<strong>Set &quot;<a href="./?email=' . \$to . '&send=true">'
+		 . './?email=' . \$to . '&send=true</a>&quot; to send a test e-mail.</strong>';
+	};
+	
+	if ( isset( \$success ) )
+	{	
+		echo 'E-mail sent to: ' . \$to;
+		echo '<br />';
+		echo 'Successful mail?: <strong ' . ( \$success ? 'style="color:green;">YES' : 'style="color:red;">NO' ) . '</strong>';
+	}
+	else
+	{
+		echo '<br />';
+		echo 'E-mail set as: '.\$to;
+	};
+	
+	echo '<hr />';	
+	echo '<style>	* { font-family: Helvetica, Arial, sans-serif;  } th { text-align: left; } td { padding: 3px 5px; }	</style>';
+	echo '<table>';	
+	
+	foreach ( \$server as \$s )
+	{
+		echo '<tr><th>\$_SERVER[\'' . \$s . '\']</th><td>' . \$_SERVER[\$s] . '</td></tr>';
+	};
+	
+	echo '</table>';
+	
+	if ( isset( \$success ) )
+	{
+		echo '<!--'; 
+		var_dump( \$success );		
+		echo '-->';
+	};
+?>
+
+EOT
 
 # Permission
 sudo chown -R apache:vagrant /var/www/html
--- centos6-64bit/provision/middleware/mailcatcher.sh
+++ centos6-64bit/provision/middleware/mailcatcher.sh
@@ -1,30 +1,113 @@
-gem install mailcatcher
-sudo yum -y install sqlite-devel
-#mailcatcher --http-ip 192.168.33.10  --smtp-ip 192.168.33.10 --no-quit
-mailcatcher --http-ip 192.168.33.10 --smtp-ip 192.168.33.10
-#mailcatcher --http-ip 192.168.33.10 --smtp-ip 192.168.33.10 -v -f > /var/log/mailcatcher.log
 sudo setsebool -P httpd_can_network_connect on
 sudo setsebool -P httpd_can_sendmail on
 sudo  setenforce Permissive
+
+gem install mailcatcher
+#sudo yum -y install sqlite-devel
+
+#wget -q  http://blog.starcklin.com/files/dpkg_1.17.6.tar.xz
+
+#
+# https://gist.github.com/EHLOVader/9233230
+#
+sudo cat << EOT > /etc/init.d/mailcatcher
+#!/bin/bash
+#chkconfig: 2345 85 15
+#descpriction: apache(httpd) Web Server
+
+PID_FILE=/var/run/mailcatcher.pid
+NAME=mailcatcher
+PROG="/usr/bin/env mailcatcher"
+USER=mailcatcher
+GROUP=mailcatcher
+
+start() {
+        echo -n "Starting MailCatcher"
+        if start-stop-daemon --stop --quiet --pidfile \$PID_FILE --signal 0
+        then
+                echo " already running."
+                exit
+        fi
+        start-stop-daemon \
+                --start \
+                --pidfile \$PID_FILE \
+                --make-pidfile \
+                --background \
+                --exec \$PROG \
+                --user \$USER \
+                --group \$GROUP \
+                --chuid \$USER \
+                -- \
+                --foreground \
+                --http-ip=0.0.0.0 \
+                --http-port=1080 \
+                --smtp-port=1025
+        echo "."
+        return \$?
+}
+
+stop() {
+        echo -n "Stopping MailCatcher"
+        start-stop-daemon \
+                --stop \
+                --oknodo \
+                --pidfile \$PID_FILE
+        echo "."
+        return \$?
+}
+
+restart() {
+        stop
+        start
+}
+
+case "\$1" in
+        start)
+                start
+                ;;
+        stop)
+                stop
+                ;;
+        restart)
+                restart
+                ;;
+        *)
+                echo "Usage: \$0 {start|stop|restart}"
+                exit 1
+                ;;
+esac
+EOT
+#sudo chmod +x mailcatcher
+#sudo chkconfig --add mailcatcher
+#sudo chkconfig mailcatcher on
+
+
+sudo mkdir /var/log/mailcatcher
+sudo chmod 775 /var/log/mailcatcher
+sudo touch /var/log/mailcatcher/mailcatcher.log
+sudo chown -R vagrant:vagrant /var/log/mailcatcher
+
+#mailcatcher --http-ip 192.168.33.10  --smtp-ip 192.168.33.10 --no-quit
+#mailcatcher --http-ip 192.168.33.10 --smtp-ip 127.0.0.1 --smtp-port 1025
+#mailcatcher --http-ip 192.168.33.10 --smtp-ip 127.0.0.1  --smtp-port 1025 -v -f > /var/log/mailcatcher.log
+/opt/rbenv/shims/mailcatcher --http-ip 0.0.0.0 --smtp-ip 0.0.0.0 -v -f > /var/log/mailcatcher/mailcatcher.log&
 
 sudo cat << EOT > /etc/httpd/conf.d/mailcatcher.conf
 <VirtualHost *:80>
   ServerName mailcatcher.vagrant-vm.dev
 
-  #ProxyRequests Off
   ProxyPass /assets http://192.168.33.10:1080/assets
-  ProxyPass /messages ws://192.168.33.10:1080/messages
+  ProxyPass /messages http://192.168.33.10:1080/messages
 
   <Directory />
     Options FollowSymLinks
     AllowOverride None
     ProxyPass http://192.168.33.10:1080
-    ProxyPassReverse http://192.168.33.10:1080
   </Directory>
 
 </VirtualHost>
 EOT
-sudo sed -i -e "s/sendmail_path = \/usr\/sbin\/sendmail -t -i/sendmail_path = \/usr\/bin\/env \/opt\/rbenv\/shims\/catchmail --smtp-ip 192.168.33.10 --smtp-port 1025/g" /etc/php.ini
+sudo sed -i -e "s/sendmail_path = \/usr\/sbin\/sendmail -t -i/sendmail_path = \/usr\/bin\/env \/opt\/rbenv\/shims\/catchmail/g" /etc/php.ini
 
 sudo /etc/init.d/httpd restart
 
