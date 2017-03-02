c29bbb83bcc5087f6e0e49990a405f96eca28039
artlife3 <artlife3@mac.com>
2017-03-03 03:54:04 +0900

    Add judgment logic on whether it is installed in the installer script #21

--- install/MacOSX/00_install_vagrant.sh
+++ install/MacOSX/00_install_vagrant.sh
@@ -1,9 +1,45 @@
 #!/bin/sh
+
+# brew
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    # https://github.com/mxcl/homebrew/wiki/installation
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+else
+    brew update
+fi
+
+# Brew cask
 brew install caskroom/cask/brew-cask
-brew cask install virtualbox
-brew cask install vagrant
+
+# virtualbox
+which -s virtualbox
+if [[ $? != 0 ]] ; then
+    brew cask install virtualbox
+else
+    brew cask info virtualbox
+fi
+
+# vagrant
+which -s vagrant
+if [[ $? != 0 ]] ; then
+    brew cask install vagrant
+else
+    brew cask info vagrant
+fi
+
+# vagrant
+which -s vagrant
+if [[ $? != 0 ]] ; then
+    brew cask install vagrant
+else
+    brew cask info vagrant
+fi
+
 brew cask install vagrant-manager
 vagrant --version
+brew cask cleanup
 
 echo " Installation of vagrant command completed."
 echo ""
--- install/MacOSX/10_create_centos6-64bit_box.sh
+++ install/MacOSX/10_create_centos6-64bit_box.sh
@@ -4,14 +4,21 @@ os_name="centos6-64bit"
 box_url="https://github.com/tommy-muehle/puppet-vagrant-boxes/releases/download/1.0.0/centos-6.6-x86_64.box"
 
 vagrant plugin install vagrant-hostsupdater
-vagrant init ${os_name}
-vagrant box add ${os_name} ${box_url}
 
-rm Vagrantfile
-mv .vagrant/ ../../centos6-64bit/
-cd ../../centos6-64bit/
+if [  -e ../../centos6-64bit/.vagrant ]; then
+    vagrant init ${os_name}
+    vagrant box add ${os_name} ${box_url}
 
-echo " Completion of virtual box creation."
+    rm Vagrantfile
+    mv .vagrant/ ../../centos6-64bit/
+    echo ""
+    echo " Completion of virtual box creation."
+else
+    echo ""
+    echo " Already exists.No change."
+    echo " Or delete the \". Vagrant\" folder."
+fi
+
 echo ""
 echo "==== Execute these commands. ==="
 echo ""
