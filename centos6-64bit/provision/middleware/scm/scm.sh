820c1663d895049b8a7613a052229e31d46f3805
artlife3 <artlife3@mac.com>
2017-03-07 21:47:29 +0900

    Changed version of git from 1.7 to 2.x #31

--- centos6-64bit/provision/middleware/scm/scm.sh
+++ centos6-64bit/provision/middleware/scm/scm.sh
@@ -1,8 +1,33 @@
 #SCM
 echo "---------- SCM ----------"
 
-sudo yum -y install git
-sudo yum -y install svn
-sudo yum -y install hg
+which -s git
+if [[ $? != 0 ]] ; then
+  echo "---------- Install: Git ----------"
+  cd /etc/yum.repos.d/
+  sudo wget wget http://wing-repo.net/wing/6/EL6.wing.repo
+  sudo yum  --enablerepo=wing -y install git
+  git --version
+else
+  git --version
+fi
+
+
+which -s svn
+if [[ $? != 0 ]] ; then
+  echo "---------- Install: SVN ----------"
+  sudo yum -y install svn
+else
+  svn --version
+fi
+
+which -s hg
+if [[ $? != 0 ]] ; then
+  echo "---------- Install: HG ----------"
+  sudo yum -y install hg
+else
+  hg --version
+fi
+
 
 
