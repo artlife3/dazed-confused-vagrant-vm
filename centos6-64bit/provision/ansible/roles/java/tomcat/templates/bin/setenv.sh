3b6715a8afe11642a1cd0a9614d3d1f909631683
Atsushi Iwasaki <a_iwasaki@PANU-MBP.local>
2017-05-09 19:33:17 +0900

    Tomcat 7 and 8 can be selected #41

--- centos6-64bit/provision/ansible/roles/java/tomcat/templates/bin/setenv.sh
+++ centos6-64bit/provision/ansible/roles/java/tomcat/templates/bin/setenv.sh
@@ -1,2 +1,2 @@
 JAVA_HOME=/usr/java/default
-JAVA_OPTS="-Xms512M -Xmx512M"
+JAVA_OPTS="-Xms1024M -Xmx1024M -XX:MaxPermSize=256m -Xloggc:{{ tomact_dir }}/{{ tomcat_name }}/logs/tomcat-gc.log -XX:+PrintGCDetails"
